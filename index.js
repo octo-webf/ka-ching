95378d28432741cdcb385c625fced81484ac9bd5
François Petitit <fpetitit@octo.com>
2015-09-23 14:33:56 +0200

    back-end first APIs

---
+++ index.js
@@ -0,0 +1,66 @@
+var express = require("express");
+var Datastore = require("nedb");
+var _ = require("lodash");
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var expressJwt = require('express-jwt');
+var _SECRET = "secret_a_changer";
+var jwt = require('jsonwebtoken');
+
+var accountsDB = new Datastore({filename: ".//database/accounts.db", autoload: true});
+var friendsDB = new Datastore({filename: ".//database/friends.db", autoload: true});
+var usersDB = new Datastore({filename: ".//database/users.db", autoload: true});
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+
+
+app.get("/", function (req, res, next) {
+  res.status(200).send("Welcome to the Ka-ching Application");
+});
+//app.get("/home", appController.getHomePage);
+
+app.post('/authenticate', function (req, res) {
+  if (req.body !== undefined) {
+    usersDB.findOne({username: req.body.login, password: req.body.password}, function (err, profile) {
+      if (err || _.isEmpty(profile)) {
+        res.send(401, 'Wrong user or password');
+      }
+      var token = jwt.sign(profile, _SECRET, {expiresInMinutes: 60 * 5});
+      res.json({username: profile.username, token: token});
+    })
+  }
+});
+
+app.get("/api/friends", expressJwt({secret: _SECRET}), function (req, res, next) {
+  friendsDB.find({username: "john.doe"}, function (err, docs) {
+    if (err) {
+      return callback(err);
+    }
+    if (docs === undefined || docs.length === 0) {
+      res.status(404).send();
+      return;
+    }
+    res.json([docs[0].friends]);
+  })
+});
+
+app.get('/api/account', expressJwt({secret: _SECRET}), function(req, res, next){
+  accountsDB.find({username: "john.doe"}, function (err, docs) {
+    if (err) {
+      return callback(err);
+    }
+    if (docs === undefined || docs.length === 0) {
+      res.status(404).send();
+      return;
+    }
+    res.json([docs[0].account]);
+  })
+});
+
+app.listen(3000, function () {
+  console.info('running on port 3000');
+});

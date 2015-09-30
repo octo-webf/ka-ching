c6eee95d8183707f7175e8f6ba28d6a522de641f
Anass Oumiri <aoumiri@octo.com>
2015-09-30 17:41:01 +0200

    Code Uglyfication

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,47 +1,38 @@
-var app = angular.module("ka-ching");
+var app=angular.module("ka-ching");
 
-app.controller("HomeCtrl", function ($scope, $rootScope, $http, $window) {
-  $scope.username = $window.sessionStorage.username;
-  $scope.showModal = false;
-  $scope.showTransferHistory = false;
+app.controller("HomeCtrl",function ($scope,$rootScope,$http,$window)
+{
+  $scope.username=     $window.sessionStorage.username;
+    $scope.showModal =false;
+  $scope.showTransferHistory=false;
 
-  $http({url: "http://localhost:3000/api/friends", method: "GET"}).then(function (response) {
-    $scope.friends = response.data;
-  }, function () {
-    $scope.error = "Impossible de retrouver vos amis";
-  });
+  $http({url: "http://localhost:3000/api/friends",method:"GET"}).then(function (response) {
+    $scope.friends=response.data;},
+  function(){$scope.error="Impossible de retrouver vos amis";});
 
-  $http({url: "http://localhost:3000/api/account", method: "GET"}).then(function (response) {
-    $scope.balance = response.data.balance;
-  });
+  $http({url:"http://localhost:3000/api/account",method: "GET"}).then(
+  function(response){$scope.balance=response.data.balance;});
 
-  $scope.addFriend = function () {
-    if ($scope.showModal == true) {
-      $scope.showModal = false
-    } else if ($scope.showModal == false) {
-      $scope.showModal = true;
-    }
-    $scope.friend = {
-      user: {
-        name: {
-          first: undefined,
-          last: undefined
-        },
-        account: {
-          number: undefined
-        }
-      }
-    }
-  };
+  $scope.addFriend=function () {
+  if ($scope.showModal == true) {
+    $scope.showModal = false
+  } else if ($scope.showModal==false){$scope.showModal = true;}
+  $scope.friend={user:{
+         name:{first:undefined,
+           last:undefined
+     },account:{number:
+  undefined }}}};
 
   $scope.submit = function (friend) {
-    $http({url: "http://localhost:3000/api/friends", method: "PUT", data: {friends: [friend]}})
-      .then(function (response) {
-        $scope.friends = response.data;
-        $scope.showModal = false;
-      }, function () {
-        $scope.error = "Impossible de d'ajouter l'ami";
-      });
+  $http({url: "http://localhost:3000/api/friends", method: "PUT", data: {friends: [friend]}})
+  .then(function (response) {
+  $scope.friends = response.data;
+
+
+  $scope.showModal = false;
+  }, function () {
+    $scope.error = "Impossible de d'ajouter l'ami";
+  });
   };
 
   $scope.transferMoney = function (friend) {
@@ -49,55 +40,58 @@ app.controller("HomeCtrl", function ($scope, $rootScope, $http, $window) {
     $http({
       url: "http://localhost:3000/api/transfers",
       method: "PUT",
-      data: {transfer: {recipient: friend, amount: friend.transferedAmount, date: new Date()}}
-    }).then(function () {
+      data: {transfer: {recipient: friend, amount: friend.transferedAmount, date: new Date()}}}).then(
+
+  function () {
       $http({url: "http://localhost:3000/api/account", method: "GET"}).then(function (response) {
         $scope.balance = response.data.balance;
         friend.transferedAmount = undefined;
-      });
-    });
+      }); });
   };
 
   $scope.transferHistory = function () {
     $scope.showTransferHistory = !$scope.showTransferHistory;
-    $http({url: "http://localhost:3000/api/transfers", method: "GET"}).then(function (response) {
-      $scope.transfers = response.data;
-    }, function () {
-      $scope.error = "Impossible de retrouver l'historique des transferts";
-    });
-  };
+    $http({url: "http://localhost:3000/api/transfers", method: "GET"}).then(
 
-  $scope.orderTransfers = function (transfers, orderByDateDescending, orderByAmountDescending, groupedByRecipient) {
+
+function (response) {
+  $scope.transfers = response.data;
+}, function () {
+  $scope.error = "Impossible de retrouver l'historique des transferts";
+});
+};
+
+  $scope.orderTransfers = function (transfers,orderByDateDescending,orderByAmountDescending,groupedByRecipient) {
     //reset order
-    transfers = _.sortByOrder(transfers, ["id"], ["desc"]);
-    var iteratees = ["date"];
-    var orders = [];
-    if (orderByDateDescending) {
-      orders = ["desc"];
-    } else {
-      orders = ["asc"];
+  transfers = _.sortByOrder(transfers, ["id"], ["desc"]);
+  var iteratees = ["date"];
+    var orders=[];
+  if (orderByDateDescending) {
+   orders = ["desc"];
+  }
+  else {
+  orders = ["asc"];
+  }
+  if (orderByAmountDescending) {
+   iteratees.unshift("amount");
+   orders.unshift("desc");
+  } if (groupedByRecipient) {
+    iteratees.unshift("recipient");
+    orders.unshift("asc");
     }
-    if (orderByAmountDescending) {
-      iteratees.unshift("amount");
-      orders.unshift("desc");
-    }
-    if (groupedByRecipient) {
-      iteratees.unshift("recipient");
-      orders.unshift("asc");
-    }
-    $scope.transfers = _.sortByOrder(transfers, iteratees, orders);
-  };
+  $scope.transfers = _.sortByOrder(transfers, iteratees, orders);
+};
 
-  $scope.toggleFriendTransfers = function (friend) {
-    friend.showTransfers = !Boolean(friend.showTransfers);
+    $scope.toggleFriendTransfers=function (friend) {
+    friend.showTransfers=!Boolean(friend.showTransfers);
     if (friend.showTransfers) {
-      $http({url: "http://localhost:3000/api/transfers/" + friend.user.username, method: "GET"}).then(function (response) {
-        friend.transfers = response.data;
-      }, function () {
-        $scope.error = "Impossible de retrouver l'historique des transferts de cet ami";
-      });
+  $http({url: "http://localhost:3000/api/transfers/"+friend.user.username,method: "GET"}).then(function (response) {
+    friend.transfers=response.data;
+  }, function () {
+    $scope.error="Impossible de retrouver l'historique des transferts de cet ami";
+  });
     }
-  };
+    };
   /*$scope.sortFriendTransfer(friend, byDate, byAmount) {
 
 

d0b733b7a44922d7a8d645303030e1638267965e
François Petitit <fpetitit@octo.com>
2015-10-03 10:16:32 +0200

    ajout de l'exécution des tests unitaires avec gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require("gulp");
 var eslint = require("gulp-eslint");
+var jasmine = require('gulp-jasmine');
+var karma = require('gulp-karma');
+
 
 gulp.task("watch", function () {
   gulp.watch(["app/scripts/**/*.js"], ["eslint"]);
@@ -10,6 +13,23 @@ gulp.task("eslint", function () {
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failOnError());
+});
+
+gulp.task('test', function() {
+  // Be sure to return the stream
+  // NOTE: Using the fake './foobar' so as to run the files
+  // listed in karma.conf.js INSTEAD of what was passed to
+  // gulp.src !
+  return gulp.src('./foobar')
+      .pipe(karma({
+        configFile: 'karma.conf.js',
+        action: 'run'
+      }))
+      .on('error', function(err) {
+        // Make sure failed tests cause gulp to exit non-zero
+        console.log(err);
+        this.emit('end'); //instead of erroring the stream, end it
+      });
 });
 
 gulp.task("default", ["eslint", "watch"]);
--- karma.conf.js
+++ karma.conf.js
@@ -1,37 +1,67 @@
+// Karma configuration
+// Generated on Sat Oct 03 2015 10:07:02 GMT+0200 (CEST)
+
 module.exports = function (config) {
-  config.set({
-    // base path, that will be used to resolve files and exclude
-    basePath: ".",
+    config.set({
 
-    // list of files / patterns to load in the browser
-    files: [
-      "app/vendors/angular/angular.js",
-      "app/vendors/angular-route/angular-route.js",
-      "app/vendors/angular-mocks/angular-mocks.js",
-      "app/vendors/jquery/dist/jquery.js",
-      "app/vendors/bootstrap/js/modal.js",
-      "app/vendors/lodash/lodash.js",
-      "app/scripts/app.js",
-      "app/scripts/controllers/*.js",
-      "app/scripts/services/*.js",
-      {pattern: "test/**/*.js", watched: true, included: true, served: true}
-    ],
+        // base path that will be used to resolve all patterns (eg. files, exclude)
+        basePath: '',
 
-    autoWatch: true,
 
-    frameworks: ["mocha"],
+        // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: ['jasmine'],
 
-    browsers: [
-      "PhantomJS"
-    ],
 
-    plugins: ["karma-mocha"],
+        // list of files / patterns to load in the browser
+        files: [
+            'app/vendors/angular/angular.js',
+            'app/vendors/angular-mocks/angular-mocks.js',
+            'app/vendors/angular-route/angular-route.js',
+            'app/scripts/**/*.js',
+            'test/**/*spec.js'
+        ],
 
-    // If browser does not capture in given timeout [ms], kill it
-    captureTimeout: 60000,
 
-    // Continuous Integration mode
-    // if true, it capture browsers, run tests and exit
-    singleRun: false
-  });
-};
+        // list of files to exclude
+        exclude: [],
+
+
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {},
+
+
+        // test results reporter to use
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        reporters: ['progress'],
+
+
+        // web server port
+        port: 9876,
+
+
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
+
+
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
+
+
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
+
+
+        // start these browsers
+        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+        browsers: ['PhantomJS'],
+
+
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun: true
+    })
+}
